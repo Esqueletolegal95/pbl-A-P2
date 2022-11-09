8ba8d116ea062257ffcbffd1a8b5dc712e5cf57c
tiagomoura28 <39248246+tiagomoura28@users.noreply.github.com>
2022-11-08 22:26:04 -0300

    Add files via upload

--- PBL A&P 2/src/controller/GrupoController.java
+++ PBL A&P 2/src/controller/GrupoController.java
@@ -1,42 +1,70 @@
 package controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
+
 import model.Selecao;
-import model.SelecaoDAO;
-import model.SelecaoImplDao;
+//import model.SelecaoDAO;
+//import model.SelecaoImplDao;
 import model.Grupo;
 import model.GrupoDao;
-import model.GrupoImplDao;
+
 
 public class GrupoController {
-	GrupoDao grupodao = new GrupoImplDao();
-	SelecaoDAO selecaodao = new SelecaoImplDao();
+
+	Singleton menu = new Singleton();
 	
 	public void criarGrupo(String nome) {
-		grupodao.criarGrupo(nome);
+		menu.getGrupodao().criarGrupo(nome);
+		
 	}
 	
 	
 	
 	public void excluirGrupo(int id) {
-		grupodao.excluirGrupo(id);
+		menu.getGrupodao().excluirGrupo(id);
+		
+		
 	}
 	
-	public List<Grupo> listarGrupo(){
-		return grupodao.listarGrupo();
+	public String listarGrupo(){
+			
+		
+		 
+		String s= "";
+		for(Grupo x: menu.getGrupodao().listarGrupo()) {
+			ArrayList<String> nomes = new ArrayList<String>();
+			for(int i=0;i<4;i++) {
+			
+			
+		nomes.add(menu.getSelecaodao().retornaSelecao(x.getSelecoes().get(i)).getNome());
+			
+			}
+			
+				System.out.println("|Id: "+x.getId()+"|Nome: "+x.getNome()+"|Selecao1: "+ nomes.get(0) +"|Selecao2: "+nomes.get(1)+"|Selecao3: "+nomes.get(2)+"|Selecao4: "+nomes.get(3)+"|\n");
+			
+		}
+		return s;
+	
+		
+				
 	}
 	
 	public GrupoDao getGrupodao(){
-		return grupodao;
+		return menu.getGrupodao();
+				
+			
 		
 	}
 	
 	public List<Selecao> listarSelecao(int id){
 		
+		//return selecaodao.IdsParaSelecoes(grupodao.retornaGrupo(id).getSelecoes());
 				
-				
-			return selecaodao.IdsParaSelecoes(grupodao.retornaGrupo(id).getSelecoes());
+			return menu.getSelecaodao().IdsParaSelecoes(menu.getGrupodao().retornaGrupo(id).getSelecoes());
+					
+					
 					
 	
 					
@@ -46,11 +74,13 @@ public class GrupoController {
 		
 	}
 	public void excluirSelecao(int idgrupo, int id) {
-		for(Grupo grupo: grupodao.listarGrupo()) {
+		
+		for(Grupo grupo: menu.getGrupodao().listarGrupo()) {
 			if(grupo.getId()== idgrupo) {
 				for(Integer Time : grupo.getSelecoes()) {
 					if(Time==id) {
-						grupodao.retornaGrupo(id).getSelecoes().remove(id);						
+						menu.getGrupodao().retornaGrupo(id).getSelecoes().remove(id);
+						
 					}				
 			}
 		}
@@ -59,6 +89,7 @@ public class GrupoController {
 		
 
 	public Grupo retornaGrupo(int id) {
-		return grupodao.retornaGrupo(id);
+		return menu.getGrupodao().retornaGrupo(id);
+				
 	}
 }
--- PBL A&P 2/src/controller/PartidaController.java
+++ PBL A&P 2/src/controller/PartidaController.java
@@ -1,65 +1,137 @@
 package controller;
 
+import java.util.ArrayList;
+
 import java.util.Date;
 import java.util.List;
 
 import model.Partida;
 import model.PartidaDAO;
-import model.PartidaImplDao;
+//import model.PartidaImplDao;
 import model.Selecao;
-import model.SelecaoDAO;
-import model.SelecaoImplDao;
+import model.Entidade;
+//import model.SelecaoDAO;
+//import model.SelecaoImplDao;
 import model.Grupo;
-import model.GrupoDao;
-import model.GrupoImplDao;
+//import model.GrupoDao;
+//import model.GrupoImplDao;
+import model.Jogador;
 
 public class PartidaController {
-	PartidaDAO partidadao = new PartidaImplDao();
-	GrupoDao grupodao = new GrupoImplDao();
-	SelecaoDAO selecaodao = new SelecaoImplDao();
-	
-	public void inserirPartida(Date data2, int horario2, String local2, int IdTime1, int idTime2, int golsTime1,
+	//PartidaDAO partidadao = new PartidaImplDao();
+	//GrupoDao grupodao = new GrupoImplDao();
+	//SelecaoDAO selecaodao = new SelecaoImplDao();
+	Singleton menu = new Singleton();
+	public void inserirPartida(Date data2, Date horario2, String local2, int idTime1, int idTime2, int golsTime1,
 			int golsTime2) {
-		partidadao.inserirPartida(data2, horario2, local2, IdTime1, idTime2, golsTime1, golsTime2);
+		
+		menu.getPartidadao().inserirPartida(data2, horario2, local2, idTime1, idTime2, golsTime1, golsTime2);
+		
 	}
 	
 	public void editarPartida(int id,int num, String dado) {
-		partidadao.editarPartida(id, num, dado);
+		menu.getPartidadao().editarPartida(id, num, dado);
+		//partidadao.editarPartida(id, num, dado);
 	}
 	
 	public void excluirPartida(int id) {
-		partidadao.excluirPartida(id);
+		menu.getPartidadao().excluirPartida(id);
+		//partidadao.excluirPartida(id);
 	}
 	
 	public List<Partida> listarPartida(){
-		return partidadao.listarPartida();
+		return menu.getPartidadao().listarPartida();
+				
 	}
 	
 	public PartidaDAO getPartidadao(){
-		return partidadao;
+		return menu.getPartidadao();
+				//partidadao;
 		
 	}
 	
-	public List<Selecao> listarSelecoesPartida(int id){
+	public String listarDadosPartida(int id){
 		
+
+		//return menu.getPartidadao().listarPartida();
+				 
+		String s= "";
+		for(Partida x: menu.getPartidadao().listarPartida()) {
+						
+			
+			ArrayList<String> nomes = new ArrayList<String>();
+			for(int i=0;i<2;i++) {
+			
+				nomes.add(menu.getSelecaodao().retornaSelecao(x.getTime1()).getNome());
+						
+							
+			}
+		
+			
+			
+			System.out.println("|Id: "+x.getId()+"|Data: "+x.getData()+"|Horario: "+x.getHorario()+"|Local: "+x.getLocal()+"|Time1: "+nomes.get(0)+"|Time2: "+nomes.get(1)+"|GolsTime1: "+x.getGolsTime1()+"|GolsTime2: "+x.getGolsTime2()+"|\n");
+			
+		}
+		return s;
+	
 		
 				
-			return selecaodao.IdsParaSelecoes(partidadao.retornaPartida(id).getSelecoes());
+					
 						
 		
 	}
 	public void excluirTime(int idpartida, Integer id) {
+		for(Partida partida: menu.getPartidadao().listarPartida()) {
+			if(partida.getId()== id) {
+				
+				if (partida.getTime1()==id) {
+					
+						menu.getPartidadao().retornaPartida(idpartida).setTime1(9999);
+				}
+				else if (partida.getTime2()==id) {
+					menu.getPartidadao().retornaPartida(idpartida).setTime1(9999);
+				}
+						
+					}				
+			}
+		}
+	
+		
+		
+		
+		
+		
+		/*
 		for(Partida partida: partidadao.listarPartida()) {
 			if(partida.getId()== idpartida) {
 				
-					partidadao.excluirIdSelecao(partida, id);
+					.excluirIdSelecao(partida, id);
 						
 					}
 		}
-	}
+	*/
 		
 	
 	public Grupo retornaGrupo(int id) {
-		return grupodao.retornaGrupo(id);
+		return menu.getGrupodao().retornaGrupo(id);
+				
 	}
-}
+	
+	public List<Partida> pesquisarPartidasData(String nome) {
+
+List<Partida> partidasdata = new ArrayList<Partida>();
+
+		
+		for(Partida partida:  menu.getPartidadao().listarPartida()){
+				
+			if (partida.getData().toString().equals(nome)){
+				partidasdata.add(partida);}
+		}
+		return partidasdata;
+					
+	}
+		
+		
+		
+	}
+
